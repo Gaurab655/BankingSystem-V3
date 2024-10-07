ca422ae57628919d629bbbe3d08cdd91940fbf52
gaurab.chand <gaurabchand655@gmail.com>
2024-10-07 15:08:01 +0545

    login, registration,email matched,html ,logout

--- src/main/java/Swift/bvv2/bvv2/Entity/User.java
+++ src/main/java/Swift/bvv2/bvv2/Entity/User.java
@@ -1,14 +1,13 @@
 package Swift.bvv2.bvv2.Entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Entity
-@Getter
-@Setter
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Table(name="users")
 public class User {
     @Id
@@ -17,4 +16,16 @@ public class User {
     private String fullName;
     private String email;
     private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Object getPassword() {
+        return password;
+    }
+
+    public String getFullName(){
+        return this.fullName;
+    }
 }
